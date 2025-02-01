38c51667016963126c250e57b3575ace71108a16
Arsen Shkrumelyak <arsen@nerverift.com>
2025-02-01 10:33:49 +0000

    fix bugs

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,13 @@
+// React
+import { Suspense } from "react";
+
+// Components
 import SignInClient from "./page.client";
 
 export default function SignIn() {
-	return <SignInClient />;
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<SignInClient />
+		</Suspense>
+	);
 }
--- app/(auth)/sign-in/verify/page.tsx
+++ app/(auth)/sign-in/verify/page.tsx
@@ -1,5 +1,13 @@
+// React
+import { Suspense } from "react";
+
+// Components
 import VerifyClient from "./page.client";
 
 export default function Verify() {
-	return <VerifyClient />;
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<VerifyClient />
+		</Suspense>
+	);
 }
